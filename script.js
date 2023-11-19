9e555e93f20e855cbde622f266e5c9d262a1bade
codeFabFit <fabgar14@gmail.com>
2023-11-19 15:57:28 -0500

    added javascript elements

---
+++ script.js
@@ -0,0 +1,30 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [
+    {
+        name: "rock",
+        emoji: "👊🏾",
+        beats: "scissors"
+    },
+    {
+        name: "paper",
+        emoji: "🖐🏾",
+        beats: "rock"
+    },
+    {
+        name: "scissors",
+        emoji: "✌🏾",
+        beats: "paper"
+    },
+]
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+       const selectionName = selectionButton.dataset.selection
+       const selection = SELECTIONS.find(selection => selection.name === selectionName)
+       makeSelection(selection)
+    })
+})
+
+function makeSelection(selection) {
+    console.log(selection)
+}
